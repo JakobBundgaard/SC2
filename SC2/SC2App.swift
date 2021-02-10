cd85fb9e17a758964499a251634c4cece9caba8c
Jakob Bundgaard <jakobbundgaard@hotmail.com>
2021-02-10 11:36:12 +0100

    Initial Commit

---
+++ SC2/SC2App.swift
@@ -0,0 +1,17 @@
+//
+//  SC2App.swift
+//  SC2
+//
+//  Created by Jakob Bundgaard on 10/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct SC2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
